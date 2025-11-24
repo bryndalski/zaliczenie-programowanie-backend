afb696c044216fe36ab48b327df5b58ec32e3e40
bryndalski <kubabryndal@gmail.com>
2025-11-24 16:46:14 +0100

    chore: remove .next from git tracking

--- front-end/.next/dev/static/chunks/src_app_layout_tsx_78cdd4a3._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__0f0ba101._.css",
-  "static/chunks/node_modules_5e571901._.js",
-  "static/chunks/src_bb6d6e5d._.js"
-],
-    source: "dynamic"
-});
